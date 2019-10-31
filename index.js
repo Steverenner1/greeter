5203f1536f5e694a36864bbf6284c7756b66884e
Steve Renner <che1111@hotmail.com>
2019-10-31 14:52:42 -0700

    Added PORT

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ server.get('/', (req, res) => {
 });
 
 // the port is coming from the environment
-const port = process.env.PORT || 4000; //making the port dynamic
+const PORT = process.env.PORT || 4000; //making the port dynamic
 // the PORT will be set by heroku when we deploy
 
-server.listen(4000, () => {
-  console.log('\n Server running on port 4000 \n');
-});
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
